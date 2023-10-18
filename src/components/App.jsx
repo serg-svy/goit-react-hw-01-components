72c46d0a8d3f10b91d4deed685f09c2eb4e21b72
serg-svy <97796536+serg-svy@users.noreply.github.com>
2023-10-18 20:29:29 +0300

    push

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,24 +1,16 @@
-import { Profile } from 'components/Profile/Profile';
-import { Statistics } from 'components/Statistics/Statistics';
-import { FriendList } from 'components/FriendList/FriendList';
-import { TransactionHistory } from 'components/TransactionHistory/TransactionHistory';
-import user from 'backEndData/user.json';
-import data from 'backEndData/data.json';
-import friends from 'backEndData/friends.json';
-import transactions from 'backEndData/transactions.json';
 export const App = () => {
   return (
-    <>
-      <Profile
-        username={user.username}
-        tag={user.tag}
-        location={user.location}
-        avatar={user.avatar}
-        stats={user.stats}
-      />
-      <Statistics stats={data} />
-      <FriendList friends={friends} />
-      <TransactionHistory items={transactions} />
-    </>
+    <div
+      style={{
+        height: '100vh',
+        display: 'flex',
+        justifyContent: 'center',
+        alignItems: 'center',
+        fontSize: 40,
+        color: '#010101'
+      }}
+    >
+      React homework template
+    </div>
   );
 };
